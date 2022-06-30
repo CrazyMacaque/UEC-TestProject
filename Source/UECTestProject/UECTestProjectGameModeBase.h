834c7f86a055df2ccdac8b050d6f922a9549d8e1
CrazyMacaque <orlandokyo@outlook.com>
2022-06-30 18:07:56 +0800

    CreateProject
    P3 1:04

---
+++ Source/UECTestProject/UECTestProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UECTestProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UECTESTPROJECT_API AUECTestProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
